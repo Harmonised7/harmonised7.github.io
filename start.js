d8ba72005b1bdf168fe2b2b69aef628d4bd3e5aa
Harmonised <harmonised7@gmail.com>
2020-11-04 23:17:18 +0000

    index test 3

--- start.js
+++ start.js
@@ -1,35 +1,53 @@
 const fs = require( "fs" );
 
 var startFolderName = __dirname;
-const indexes = fs.readdirSync( "./" );
-while( startFolderName.includes( "\\" ) )
+const blacklist = 
+[
+    __filename,
+    ".git",
+    "index.html"
+];
+
+function createIndex( dir )
 {
-    startFolderName = startFolderName.slice( startFolderName.indexOf( "\\" ) + 1 );
+    console.log( `indexing ${dir}` );
+    var startFolderName = dir;
+    const indexes = fs.readdirSync( dir );
+    while( startFolderName.includes( "\\" ) )
+    {
+        startFolderName = startFolderName.slice( startFolderName.indexOf( "\\" ) + 1 );
+    }
+
+    var indexList = "";
+
+    indexes.forEach( index =>
+    {
+        if( !blacklist.includes( index ) )
+        {
+            var stat = fs.statSync( `${dir}/${index}` );
+            if( stat.isDirectory() )
+                createIndex( `${dir}/${index}` );
+
+            indexList += `\t\t\t<li>\n`;
+            indexList += `\t\t\t\t<a href='${index}'>${index}</a>\n`;
+            indexList += `\t\t\t</li>\n`;
+        }
+    });
+
+    const output = 
+    `<!DOCTYPE html>
+    <html>
+    <head><title>Index of ./${startFolderName}</title></head>
+    <body>
+        <h2>Index of ./${startFolderName}</h2>
+        <hr>
+        <ul>
+    ${indexList}
+        </ul>
+    </body>
+    </html>`;
+
+    fs.writeFileSync( `${dir}/index.html`, output );
 }
 
-indexList = "";
-
-indexes.forEach( index =>
-{
-    if( index != __filename )
-    {
-        indexList += `\t\t\t<li>\n`;
-        indexList += `\t\t\t\t<a href='${index}'>${index}</a>\n`;
-        indexList += `\t\t\t</li>\n`;
-    }
-});
-
-const output = 
-`<!DOCTYPE html>
-<html>
-<head><title>Index of ./${startFolderName}</title></head>
-<body>
-    <h2>Index of ./${startFolderName}</h2>
-    <hr>
-    <ul>
-${indexList}
-    </ul>
-</body>
-</html>`;
-
-fs.writeFileSync( "./index.html", output );
+createIndex( __dirname );
